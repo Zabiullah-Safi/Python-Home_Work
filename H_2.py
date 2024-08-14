1e952d4416d3af7db657e8394fcfb61fef11874b
Zabiullah-Safi <zabiullahshams00@gmail.com>
2024-08-13 23:40:04 -0700

    Update H_2.py

--- H_2.py
+++ H_2.py
@@ -1,3 +1,27 @@
+
+# 16: Create a Library with attributes name and books(a list of Book object). Provide methods
+# to add and remove books.
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add(self, book):
+        return self.books.append(book)
+
+    def remove(self, book):
+        return self.books.remove(book)
+
+    def detail(self):
+        print(f"{self.name}\n{self.books}")
+l = Library("Library")
+l.add("1 added")
+l.add("2added")
+l.detail()
+l.remove("1 added")
+l.detail()
+
 # fibonacci sequence
 
 def fibonacci(n):
